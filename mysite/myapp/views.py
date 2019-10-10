a6542b1e826b1d44220ec3835a0e16a71a6f9271
mhoshko <madeline.hoshko@gmail.com>
2019-10-09 19:19:19 -0700

    Added both views.py and trending.html with additions of looping through articles

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -13,16 +13,26 @@ def home(request):
         self.phone = phone
 
     newCard = [
-      Card("American Red Cross", "https://www.redcross.org", "media/redCross.jpg", "2125 East Onstott Road Yuba City, CA 95991","(530) 673-1460"),
+      Card("Red Cross", "https://www.redcross.org", "media/redCross.jpg", "2125 East Onstott Road Yuba City, CA 95991","(530) 673-1460"),
       Card("Neighborhood Church of Chico", "http://www.ncchico.org/", "media/NC.jpg", "2801 Notre Dame Boulevard Chico, CA 95928", "(530) 343-6006"),
       Card("Northern Valley Catholic Social Service", "https://www.redcross.org", "media/NVCSS.png", "2400 Washington Ave Redding, CA 96001-2832", "(530) 345-1600")
     ]
 
+    class Article:
+      def __init__(self, url, title, picUrl, summary):
+        self.url = url
+        self.title = title
+        self.picUrl = picUrl
+        self.summary = summary
+    newArticle = [
+      Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Six months after California’s Camp Fire, survivors still struggle to find temporary homes", "static/media/fire.jpg", "The blaze in Northern California’s Butte County killed 86 people, destroyed about 14,000 homes and left most of the town of Paradise in ruins. It ranks as the deadliest and most destructive fire in California history."),
+      Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Hurricane Dorian Damage in Bahamas Explained", "static/media/fire.jpg", "Meteorologist Kait Parker says the damage in the Bahamas was caused by a combination of storm surge and winds.")
+    ]
     context = {
       "cards": newCard,
-      "ranger": range(0, 3),
+      "articles": newArticle,
+      "ranger": range(0, 5),
     }
-    
     return render(request, 'main/index.html', context=context)
 
 
@@ -44,15 +54,26 @@ def agencies(request):
       "cards": newCard,
       "ranger": range(0, 3),
     }
-    
+
 
     return render(request, 'main/agencies.html', context=context)
 
 
 def trending(request):
+    class Article:
+      def __init__(self, url, title, picUrl, summary):
+        self.url = url
+        self.title = title
+        self.picUrl = picUrl
+        self.summary = summary
+    newArticle = [
+      Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Six months after California’s Camp Fire, survivors still struggle to find temporary homes", "static/media/fire.jpg", "The blaze in Northern California’s Butte County killed 86 people, destroyed about 14,000 homes and left most of the town of Paradise in ruins. It ranks as the deadliest and most destructive fire in California history."),
+      Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Hurricane Dorian Damage in Bahamas Explained", "static/media/fire.jpg", "Meteorologist Kait Parker says the damage in the Bahamas was caused by a combination of storm surge and winds.")
+    ]
     context = {
+      "articles": newArticle,
+      "ranger": range(0, 5),
     }
-    
     return render(request, 'main/trending.html', context=context)
 
 def about(request):
