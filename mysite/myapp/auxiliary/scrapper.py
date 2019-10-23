813a6b76c3682c9616afd992e9355fb36f1fe302
Austin Meyer <meyerforge@gmail.com>
2019-10-23 10:17:34 -0700

    Updated scrapper, searches news site every minute

--- mysite/myapp/auxiliary/scrapper.py
+++ mysite/myapp/auxiliary/scrapper.py
@@ -1,63 +1,42 @@
 # import libraries
 import urllib.request
+import time
 from bs4 import BeautifulSoup
+from articleInfo import ArticleInfo
 
 
-class articleInfo:
+while(1):
+  print("Starting Up")
 
-  def __init__(self, title, url):
-    self.title = title
-    self.url = url
-    self.intro = ""
+  # specifiy the url
+  quote_page = 'https://www.actionnewsnow.com/news/local/'
 
-  def __str__(self):
-    return "Title = " + self.title + "\n" + "Url = " + self.url + "\nIntro = " + self.intro
-
-  def setIntro(self, intro):
-    self.intro = intro
-
-
-
-
-def getIntro(url, article):
-
-  with urllib.request.urlopen(url) as page:
+  #query the website and return the html to the variable 'page'
+  with urllib.request.urlopen(quote_page) as page:
     html = page.read()
 
+  # parse the html using BeautifulSoup
   soup = BeautifulSoup(html, "html.parser")
-  intro = soup.find("p", attrs={"class":"article-summary"})
-  if(intro):
-    article.intro = intro.get_text()
+  betterSoup = soup.prettify()
 
-# specifiy the url
-quote_page = 'https://www.actionnewsnow.com/'
+  # Find individual newsArticles
+  newsArticles = soup.find_all("h2", attrs={"class": "entry-title"})
 
-#query the website and return the html to the variable 'page'
-with urllib.request.urlopen(quote_page) as page:
-  html = page.read()
+  # Scrape topNews
+  images = soup.find_all("a")
+  results = open('record.txt', 'w')
 
-# parse the html using BeautifulSoup
-soup = BeautifulSoup(html, "html.parser")
-betterSoup = soup.prettify()
+  articles = []
 
-# Find individual newsArticles
-newsArticles = soup.find_all("h2", attrs={"class": "entry-title"})
+  #
+  for article in newsArticles:
+    articles.append(ArticleInfo(article.find('a').get_text(), article.find('a').get('href')))
+    articles[-1].getIntro()
 
-# Scrape topNews
-images = soup.find_all("a")
-results = open('record.txt', 'w')
+    print("", articles[-1])
 
-articles = []
+    print("-------------------------------------------")
 
-#
-for article in newsArticles:
-
-  for child in article:
-    articles.append(articleInfo(child.get_text(), child.get('href')))
-    getIntro(articles[-1].url, articles[-1])
-
-    print(articles[-1])
-
-  print("-------------------------------------------")
-
+  print("Resting")
+  time.sleep(60)
 
