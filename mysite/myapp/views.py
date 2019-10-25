6abc5fe8319d3b86f764d9987d47b1618f6cebff
Austin Meyer <meyerforge@gmail.com>
2019-10-25 11:55:58 -0700

    Put in buttons for sign in and sign up, as well as cleaning up signIn.

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 import pyrebase
 
-
+# For Testing
 class Article:
   def __init__(self, url, title, picUrl, summary):
     self.url = url
@@ -18,6 +18,17 @@ class Card:
     self.address = address
     self.phone = phone
 
+newCard = [
+    Card("Red Cross", "https://www.redcross.org", "media/redCross.jpg", "2125 East Onstott Road Yuba City, CA 95991","(530) 673-1460"),
+    Card("Neighborhood Church of Chico", "http://www.ncchico.org/", "media/NC.jpg", "2801 Notre Dame Boulevard Chico, CA 95928", "(530) 343-6006"),
+    Card("Northern Valley Catholic Social Service", "https://www.redcross.org", "media/NVCSS.png", "2400 Washington Ave Redding, CA 96001-2832", "(530) 345-1600")
+  ]
+
+newArticle = [
+  Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Six months after California’s Camp Fire, survivors still struggle to find temporary homes", "static/media/fire.jpg", "The blaze in Northern California’s Butte County killed 86 people, destroyed about 14,000 homes and left most of the town of Paradise in ruins. It ranks as the deadliest and most destructive fire in California history."),
+  Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Hurricane Dorian Damage in Bahamas Explained", "static/media/fire.jpg", "Meteorologist Kait Parker says the damage in the Bahamas was caused by a combination of storm surge and winds.")
+]
+# End testing block
 
 config = {
     'apiKey': "AIzaSyDGPibcqcm-BDI-yckPYFcXpPCFvhnPd3E",
@@ -38,16 +49,7 @@ auth = firebase.auth()
 # Create your views here.
 def home(request):
   title = "Home "
-  newCard = [
-    Card("Red Cross", "https://www.redcross.org", "media/redCross.jpg", "2125 East Onstott Road Yuba City, CA 95991","(530) 673-1460"),
-    Card("Neighborhood Church of Chico", "http://www.ncchico.org/", "media/NC.jpg", "2801 Notre Dame Boulevard Chico, CA 95928", "(530) 343-6006"),
-    Card("Northern Valley Catholic Social Service", "https://www.redcross.org", "media/NVCSS.png", "2400 Washington Ave Redding, CA 96001-2832", "(530) 345-1600")
-  ]
 
-  newArticle = [
-    Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Six months after California’s Camp Fire, survivors still struggle to find temporary homes", "static/media/fire.jpg", "The blaze in Northern California’s Butte County killed 86 people, destroyed about 14,000 homes and left most of the town of Paradise in ruins. It ranks as the deadliest and most destructive fire in California history."),
-    Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Hurricane Dorian Damage in Bahamas Explained", "static/media/fire.jpg", "Meteorologist Kait Parker says the damage in the Bahamas was caused by a combination of storm surge and winds.")
-  ]
   context = {
     "title": title,
     "cards": newCard,
@@ -59,11 +61,7 @@ def home(request):
 
 def agencies(request):
   title = "Agencies "
-  newCard = [
-    Card("Red Cross", "https://www.redcross.org", "media/redCross.jpg", "2125 East Onstott Road Yuba City, CA 95991","(530) 673-1460"),
-    Card("Neighborhood Church of Chico", "http://www.ncchico.org/", "media/NC.jpg", "2801 Notre Dame Boulevard Chico, CA 95928", "(530) 343-6006"),
-    Card("Northern Valley Catholic Social Service", "https://www.redcross.org", "media/NVCSS.png", "2400 Washington Ave Redding, CA 96001-2832", "(530) 345-1600")
-  ]
+  
   context = {
     "title": title,
     "cards": newCard,
@@ -76,11 +74,6 @@ def agencies(request):
 
 def trending(request):
   title = "Trending News "
-
-  newArticle = [
-    Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Six months after California’s Camp Fire, survivors still struggle to find temporary homes", "static/media/fire.jpg", "The blaze in Northern California’s Butte County killed 86 people, destroyed about 14,000 homes and left most of the town of Paradise in ruins. It ranks as the deadliest and most destructive fire in California history."),
-    Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Hurricane Dorian Damage in Bahamas Explained", "static/media/fire.jpg", "Meteorologist Kait Parker says the damage in the Bahamas was caused by a combination of storm surge and winds.")
-  ]
 
   context = {
     "title": title,
@@ -101,17 +94,6 @@ def about(request):
 
 def signIn(request):
   title = "Sign In "
-
-  try:
-    user = auth.sign_in_with_email_and_password(email, password)
-
-  except:
-    context = {
-      "title": title,
-      "msg": "Invalid Credentials. Please try again or not. Up to you.",
-    }
-
-    return render(request, "main/signIn.html", context=context)
 
   context = {
     "title": title,
@@ -134,6 +116,7 @@ def postSignIn(request):
       "title": title,
       "msg": message,
     }
+
     return render(request, "main/signIn.html", context = context)
 
   print(user['idToken'])
