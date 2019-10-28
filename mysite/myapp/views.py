b29783dd74dc88ce646860d3799cab44d2aae5df
Austin Meyer <meyerforge@gmail.com>
2019-10-28 14:58:29 -0700

    Stripped out pyrebase goods. Started working on authentication.

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-import pyrebase
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # For Testing
 class Article:
@@ -30,20 +31,6 @@ newArticle = [
 ]
 # End testing block
 
-config = {
-    'apiKey': "AIzaSyDGPibcqcm-BDI-yckPYFcXpPCFvhnPd3E",
-    'authDomain': "donate-need.firebaseapp.com",
-    'databaseURL': "https://donate-need.firebaseio.com",
-    'projectId': "donate-need",
-    'storageBucket': "donate-need.appspot.com",
-    'messagingSenderId': "72050043145",
-    'appId': "1:72050043145:web:28ef2b2e6ff1c08bdb4bd8",
-    'measurementId': "G-ZM4B6MFKWJ"
-}
-
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
-auth = firebase.auth()
 
 
 # Create your views here.
@@ -102,15 +89,18 @@ def signIn(request):
   return render(request, "main/signIn.html")
 
 def postSignIn(request):
+  signedIn = True
   title = "Welcome "
   email = request.POST.get('email')
   passw = request.POST.get("pass")
 
-  try:
-    user = auth.sign_in_with_email_and_password(email,passw)
-  except:
+  
+  user = authenticate(email=email, password=passw)
+
+  if user is None:
     title = "Invalid "
     message = "invalid credentials"
+    signedIn = False
 
     context = {
       "title": title,
@@ -119,12 +109,10 @@ def postSignIn(request):
 
     return render(request, "main/signIn.html", context = context)
 
-  print(user['idToken'])
-  session_id = user['idToken']
-
   context = {
     "title": title,
     "e": email,
+    "signedIn": signedIn,
   }
 
   #get.session['uid']=str(session_id)
@@ -151,24 +139,21 @@ def postsignup(request):
   email = request.POST.get('email')
   passw = request.POST.get('pass')
 
-  try:
-      user = auth.create_user_with_email_and_password(email,passw)
-  except:
-    title = "Problem "
-    message = "bad signup"
+  #try:
+    # Something else
+  #except:
+  #  title = "Problem "
+  #  message = "bad signup"
 
-    context = {
-      "title": title,
-      "msg": message,
-    }
-    return render(request, "main/welcome.html", context = context)
+  #  context = {
+  #    "title": title,
+  #    "msg": message,
+  #  }
+  #  return render(request, "main/welcome.html", context = context)
 
   context = {
     "title": title,
     "e": email,
   }
   
-  uid = user['localId']
-  data = {"name":name, "status":"1"}
-  db.child("users").child(uid).child("details").set(data)
-  return render(request, "main/welcome.html", {"e":email})
+  return render(request, "main/welcome.html", context = context)
