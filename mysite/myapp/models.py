8bcbb631f77430a7e5e3558209159689ad3e78fa
mhoshko <madeline.hoshko@gmail.com>
2019-10-29 15:27:13 -0700

    updated models.py and settings.py to comment out errors

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+#from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
 class Agencies(models.Model):
@@ -8,14 +8,14 @@ class Agencies(models.Model):
   email = models.EmailField(max_length=50)
   address = models.CharField(max_length=100)
   url = models.URLField(max_length=100)
-  phone = models.PhoneNumberField()
+  #phone = models.PhoneNumberField()
 
 class Account_Page(models.Model):
-  #requests_fulfilled =
-  picture = models.ImageField(width_field=100)
+  requests_fulfilled = models.CharField(max_length=100)
+  #picture = models.ImageField(width_field=100)
 
 class Agencies_Page(models.Model):
-  causes = models.TextField(max_length=150)
+    causes = models.TextField(max_length=150)
   #requests =
 
 class Cause(models.Model):
@@ -30,7 +30,7 @@ class Request_In_Progress(models.Model):
   date_requested = models.DateField(auto_now=False)
 
 class News_Articles(models.Model):
-  picture = models.ImageField(width_field=100)
+  #picture = models.ImageField(width_field=100)
   url = models.URLField(max_length=100)
   description = models.CharField(max_length=1000)
 
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'phonenumber_field',
+    #'phonenumber_field',
 
     'myapp',
 ]
