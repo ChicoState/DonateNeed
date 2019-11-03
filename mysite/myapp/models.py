29825150127d3591db9c8ee55139fa1cfc59d79a
mhoshko <madeline.hoshko@gmail.com>
2019-11-03 11:52:40 -0800

    added models.py to include foreign keys

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -10,32 +10,32 @@ class Agencies(models.Model):
   url = models.URLField(max_length=100)
   phone = PhoneField()
 
-class Account_Page(models.Model):
-  requests_fulfilled = models.CharField(max_length=100)
+class News_Articles(models.Model):
   #picture = models.ImageField(width_field=100)
+  url = models.URLField(max_length=100)
+  description = models.CharField(max_length=1000)
 
-class Agencies_Page(models.Model):
-    causes = models.TextField(max_length=150)
-  #requests =
-
-class Cause(models.Model):
-  title = models.CharField(max_length=100)
-  location = models.CharField(max_length=100)
-  #news_articles =
+class Request_Fulfilled(models.Model):
+  fulFilledAmount = models.DecimalField(max_digits=10, decimal_places=2)
+  promisedAmount = models.DecimalField(max_digits=10, decimal_places=2)
+  promisedArrival = models.DateField(auto_now=False)
 
 class Request_In_Progress(models.Model):
   amount_total = models.DecimalField(max_digits=10, decimal_places=2)
   amount_fulfilled = models.DecimalField(max_digits=10, decimal_places=2)
   is_complete = models.BooleanField(default=False)
   date_requested = models.DateField(auto_now=False)
+  request_fulfillment = models.ForeignKey(Request_Fulfilled, on_delete=models.CASCADE)
 
-class News_Articles(models.Model):
+class Account_Page(models.Model):
+  requests_fulfilled = models.ForeignKey(Request_Fulfilled, on_delete=models.CASCADE)
   #picture = models.ImageField(width_field=100)
-  url = models.URLField(max_length=100)
-  description = models.CharField(max_length=1000)
 
+class Agencies_Page(models.Model):
+  causes = models.TextField(max_length=150)
+  requests = models.ForeignKey(Request_In_Progress, on_delete=models.CASCADE)
 
-class Request_Fulfilled(models.Model):
-  fulFilledAmount = models.DecimalField(max_digits=10, decimal_places=2)
-  promisedAmount = models.DecimalField(max_digits=10, decimal_places=2)
-  promisedArrival = models.DateField(auto_now=False)
+class Cause(models.Model):
+  title = models.CharField(max_length=100)
+  location = models.CharField(max_length=100)
+  news_articles = models.ForeignKey(News_Articles, on_delete=models.CASCADE)
