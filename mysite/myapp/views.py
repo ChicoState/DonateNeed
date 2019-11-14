df30a5f896f886f64a6758f6e96fb75ed01d6d21
Austin Meyer <meyerforge@gmail.com>
2019-11-14 13:11:16 -0800

    Scrapper with out pictures.

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -13,13 +13,6 @@ from . import models
 
 
 # For Testing
-class Article:
-  def __init__(self, url, title, picUrl, summary):
-    self.url = url
-    self.title = title
-    self.picUrl = picUrl
-    self.summary = summary
-
 class Card:
   def __init__(self, name, url, picUrl, address, phone):
     self.name = name
@@ -33,11 +26,6 @@ newCard = [
     Card("Neighborhood Church of Chico", "http://www.ncchico.org/", "media/NC.jpg", "2801 Notre Dame Boulevard Chico, CA 95928", "(530) 343-6006"),
     Card("Northern Valley Catholic Social Service", "https://www.redcross.org", "media/NVCSS.png", "2400 Washington Ave Redding, CA 96001-2832", "(530) 345-1600")
   ]
-
-newArticle = [
-  Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Six months after California’s Camp Fire, survivors still struggle to find temporary homes", "static/media/fire.jpg", "The blaze in Northern California’s Butte County killed 86 people, destroyed about 14,000 homes and left most of the town of Paradise in ruins. It ranks as the deadliest and most destructive fire in California history."),
-  Article("https://www.cnbc.com/2019/05/16/six-months-after-camp-fire-survivors-struggle-to-find-temporary-homes.html", "Hurricane Dorian Damage in Bahamas Explained", "static/media/fire.jpg", "Meteorologist Kait Parker says the damage in the Bahamas was caused by a combination of storm surge and winds.")
-]
 # End testing block 
 
 
@@ -65,6 +53,7 @@ def home(request):
   }
 
   return render(request, 'main/index.html', context=context)
+
 
 def agencies(request):
   title = "Agencies "
